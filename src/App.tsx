7e1d79c515869db012157ee29182e4b339ae04a5
Jaystarbliss studios <82984404+Jaystarbliss-studios@users.noreply.github.com>
2026-09-19 03:32:23 +0100

    chore: remove obsolete local catalogue imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Book, Chapter, Bookmark, ReadingProgress, UserProfile, LibraryFont } from './types';
 import {
-  getBooks,
-  getChapters,
   getBookBySlug,
   getChapter,
   getBookmarks,
